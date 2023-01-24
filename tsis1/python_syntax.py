05241818bb371671e360d84d856199f2273473d7
dmnv <daminov.arslan@gmail.com>
2023-01-24 15:00:55 +0600

    initial commit

---
+++ tsis1/python_syntax.py
@@ -0,0 +1,28 @@
+#Indentation
+if 5 > 2:
+  print("Five is greater than two!")
+
+#fixed indentation
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#fixed indentation
+if 5 > 2:
+ print("Five is greater than two!")
+ print("Five is greater than two!")
+
+#Variables
+x = 5
+y = "Hello, World!"
+
+#Comments
+#This is a comment.
+print("Hello, World!")
+
+#Exercise
+print("Hello, World!")
