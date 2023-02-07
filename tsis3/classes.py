1ced6f543bcc9a3fea03874661a956e7452331c2
Arslan Daminov <daminov.arslan@gmail.com>
2023-02-08 00:35:17 +0600

    updated classes

--- tsis3/classes.py
+++ tsis3/classes.py
@@ -11,23 +11,80 @@ class StringOperator:
 
 #--------SECOND--------#
 class Shape:
-    def area(shapeArea = 0):
+    def shapeArea(self, shapeArea = 0):
         print(shapeArea)
 
 class Square(Shape):
     def __init__(self, length):
         self.length = length
-        self.squareArea = length*length
-    def squareArea(self):
-        super().area(self.area)
 
-square = Square(int(input))
-square.squareArea()
+    def squareArea(self):
+        super().shapeArea(self.length*self.length)
+
+# square = Square(int(input()))
+# square.squareArea()
 
 #--------THIRD--------#
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def rectangleArea(self):
+        super().shapeArea(self.length*self.width)
+
+# rectangle = Rectangle(int(input()), int(input()))
+# rectangle.rectangleArea()
 
 #--------FOURTH--------#
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(self.x, self.y)
+    def move(self, newX, newY):
+        self.x = newX
+        self.y = newY
+    def dist(self, another):
+        print(((self.x - another.x)**2 + (self.y - another.y)**2)**0.5)
+
+# p1 = Point(1, 2)
+# p2 = Point(5, 4)
+
+# p1.show()
+# p1.move(4, 5)
+# p1.show()
+
+# p1.dist(p2)
 
 #--------FIFTH--------#
+class Account:
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Success!\nYour balance: {}".format(self.balance))
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Withdrawal is impossble: the amount is more than your balance.\nYour balance: {}".format(self.balance))
+        else:
+            self.balance -= amount
+            print("Success!\nYour balance: {}".format(self.balance))
+
+owner = Account("Freeman", 12000)
+
+owner.deposit(5000)
+
+owner.withdraw(20000)
+
+owner.withdraw(15000)
 
 #--------SIXTH--------#
+def prime_filter(numList):
+    return filter(lambda x: all(x % i != 0 for i in range(2, x)), numList)
+print(list(prime_filter([1, 2, 3, 6, 8, 10, 11])))
--- tsis3/functions1.py
+++ tsis3/functions1.py
@@ -22,7 +22,6 @@ print(solve(35, 94))
 def is_prime(num):
     for i in range(2, int(num**0.5) + 1):
         if num%i == 0: 
-            print(i)
             return False
     return True
 
